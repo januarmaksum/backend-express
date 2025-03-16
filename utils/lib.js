37fbc5adaf8ab5dbdb0306c3d493cfd97eb9890a
Januar Maksum <januarmaksum@gmail.com>
2025-03-16 14:34:57 +0700

    feat: change expires time

--- utils/lib.js
+++ utils/lib.js
@@ -1,22 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 const formatToWIB = (date) => {
-    const formatted = date.toLocaleString("id-ID", {
-      timeZone: "Asia/Jakarta",
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-    });
-  
-    return formatted.replace(/\//g, "-").replace(",", "");
+    return new Intl.DateTimeFormat('id-ID', { 
+        year: 'numeric', month: '2-digit', day: '2-digit', 
+        hour: '2-digit', minute: '2-digit', second: '2-digit', 
+        timeZone: 'Asia/Jakarta' 
+    }).format(date);
 };
 
 const generateToken = (user) => {
-    const expiresIn = 60;
+    const expiresIn = 7200; // 2 hours
     const expirationTime = new Date(Date.now() + expiresIn * 1000);
     const formattedExpirationTime = formatToWIB(expirationTime);
     
